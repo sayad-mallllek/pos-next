e2631e018f9c3ed45792ab721b3631e0dda9b25f
sayad-mallllek <ibrahim.elzein@lau.edu>
2026-01-18 13:08:05 +0200

    refactor: comment out authentication checks in proxy function for future implementation

--- proxy.ts
+++ proxy.ts
@@ -6,31 +6,31 @@ const publicRoutes = ["/", ...authRoutes];
 
 export default async function proxy(request: NextRequest) {
   // optimistic check for authentication token
-  const isAuthenticated = Boolean(
-    request.cookies.get("better-auth.session_token")
-  );
+  // const isAuthenticated = Boolean(
+  //   request.cookies.get("better-auth.session_token")
+  // );
 
-  const { pathname } = request.nextUrl;
+  // const { pathname } = request.nextUrl;
 
-  // Redirect unauthenticated users trying to access protected routes
-  if (
-    publicRoutes.some((route) => pathname.startsWith(route)) &&
-    !isAuthenticated
-  ) {
-    const url = request.nextUrl.clone();
-    url.pathname = "/login";
-    return NextResponse.redirect(url);
-  }
+  // // Redirect unauthenticated users trying to access protected routes
+  // if (
+  //   publicRoutes.some((route) => pathname.startsWith(route)) &&
+  //   !isAuthenticated
+  // ) {
+  //   const url = request.nextUrl.clone();
+  //   url.pathname = "/login";
+  //   return NextResponse.redirect(url);
+  // }
 
-  // Redirect authenticated users away from auth routes
-  if (
-    isAuthenticated &&
-    authRoutes.some((route) => pathname.startsWith(route))
-  ) {
-    const url = request.nextUrl.clone();
-    url.pathname = "/dashboard";
-    return NextResponse.redirect(url);
-  }
+  // // Redirect authenticated users away from auth routes
+  // if (
+  //   isAuthenticated &&
+  //   authRoutes.some((route) => pathname.startsWith(route))
+  // ) {
+  //   const url = request.nextUrl.clone();
+  //   url.pathname = "/dashboard";
+  //   return NextResponse.redirect(url);
+  // }
 
   return NextResponse.next();
 }
